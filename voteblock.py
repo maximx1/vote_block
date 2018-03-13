109283a24b37608f5781f02df14ff98bedae96e5
Justin Walrath <walrathjaw@gmail.com>
2018-03-13 01:20:38 -0400

    Working on transations

--- voteblock.py
+++ voteblock.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from pprint import pprint
+from Crypto.PublicKey import ECC
+from Crypto.Hash import SHA256
+from Crypto.Signature import DSS
 import hashlib
-
-def hash_block(block):
-    hash_object = hashlib.sha256(bytearray(block))
-    return hash_object.hexdigest()
 
 def validate_block_chain(bc, difficulty):
     target = "0" * difficulty
@@ -17,7 +16,42 @@ def validate_block_chain(bc, difficulty):
             return False
     return True
 
-class Block:
+def generate_key_pair(passphrase):
+    key = ECC.generate(curve='P-256')
+    private_key = key.export_key(passphrase=passphrase, format='PEM', protection="PBKDF2WithHMAC-SHA1AndAES128-CBC", compress=True)
+    public_key = key.public_key().export_key(format='PEM', compress=True)
+    return (public_key, private_key)
+
+class Citizen:
+    def __init__(self, passphrase):
+        (self.public_key, self.private_key) = generate_key_pair(passphrase)
+
+class Hashable:
+    def calculate_hash(self):
+        return self.hash_block(self.get_block_string())
+
+    def hash_block(self, block):
+        hash_object = hashlib.sha256(bytearray(block))
+        return hash_object.hexdigest()
+
+    def create_ecc_sig(self, private_key, data):
+        key = ECC.import_key(private_key)
+        converted_data = bytearray(data)
+        hashed_data = SHA256.new(converted_data)
+        signer = DSS.new(key, 'fips-186-3')
+        return signer.sign(hashed_data)
+
+    def verify_ecc_sig(self, public_key, data, signature):
+        #key = ECC.import_key(public_key)
+        hashed_message = SHA256.new(data)
+        verifier = DSS.new(public_key, 'fips-186-3')
+        try:
+            verifier.verify(hashed_message, signature)
+            return True
+        except ValueError:
+            return False
+
+class Block(Hashable):
     def __init__(self, dat, prev = "0"):
         self.nonce = 0
         self.data = dat
@@ -28,31 +62,61 @@ class Block:
     def get_block_string(self):
         return self.data + self.timestamp + self.previous + str(self.nonce)
 
-    def calculate_hash(self):
-        return hash_block(self.get_block_string())
-
     def mine_block(self, difficulty):
         target = "0" * difficulty
         while not self.hash.startswith(target):
             self.nonce += 1
             self.hash = self.calculate_hash()
 
+class Vote(Hashable):
+    def __init__(self, voter, votee, inputs):
+        self.voter = voter
+        self.votee = votee
+        self.inputs = inputs
+        self.timestamp = str(datetime.utcnow())
+        self.cast_vote_count = 1
+        self.sequence = 0
+
+    def get_block_string(self):
+        sequence += 1
+        return self.voter + self.votee + self.timestamp + str(self.cast_vote_count) + str(self.sequence)
+
+    def get_signature_data_string(self):
+        return str(self.voter) + str(self.votee) + str(self.cast_vote_count)
+
+    def sign_vote(self, private_key):
+        data = self.get_signature_data_string()
+        self.signature = self.create_ecc_sig(private_key, data)
+
+    def validate_signature(self):
+        data = self.get_signature_data_string()
+        return verify_ecc_sig(self.voter, data, self.signature)
+
+#print(Citizen("dddd").__dict__)
+
 difficulty = 6
 
-block_chain = []
+voter = Citizen("snicklefritz")
+votee = Citizen("ztirfelkcins")
+vote = Vote(voter, votee, None)
+vote.sign_vote(voter.public_key)
+print("Transaction sig is valid" if vote.validate_signature() else "Transaction sig is invalid")
 
-block_chain.append(Block("Block 1"))
-block_chain[0].mine_block(difficulty)
-print("Mined Block 1")
-
-block_chain.append(Block("Block 2", block_chain[-1].hash))
-block_chain[1].mine_block(difficulty)
-print("Mined Block 2")
-
-block_chain.append(Block("Block 3", block_chain[-1].hash))
-block_chain[2].mine_block(difficulty)
-print("Mined Block 3")
-
-print("Block chain is valid" if validate_block_chain(block_chain, difficulty) else "Block chain is invalid")
-for b in block_chain:
-    pprint(b.__dict__)
+#
+# block_chain = []
+#
+# block_chain.append(Block("Block 1"))
+# block_chain[0].mine_block(difficulty)
+# print("Mined Block 1")
+#
+# block_chain.append(Block("Block 2", block_chain[-1].hash))
+# block_chain[1].mine_block(difficulty)
+# print("Mined Block 2")
+#
+# block_chain.append(Block("Block 3", block_chain[-1].hash))
+# block_chain[2].mine_block(difficulty)
+# print("Mined Block 3")
+#
+# print("Block chain is valid" if validate_block_chain(block_chain, difficulty) else "Block chain is invalid")
+# for b in block_chain:
+#     pprint(b.__dict__)
